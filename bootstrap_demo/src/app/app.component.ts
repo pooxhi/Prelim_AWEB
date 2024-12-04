25c0d08ee0c3c53aee56e443c80ec4e42649acd4
pooxhi <rak.mariano6262@gmail.com>
2024-12-04 11:50:26 +0800

    Add files via upload

---
+++ bootstrap_demo/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, GalleryComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'prelim_lab3';
+}
